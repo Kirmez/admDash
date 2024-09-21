260877a6e5263fb7044b372ad8714f1af1e96f60
Kirill Mezentsev <kirmez02@gmail.com>
2024-09-21 18:49:30 +0200

    finished all bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Siderbar from "@/components/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,15 +28,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}>
-        <Navbar />
-        <div className="flex">
-          <div className="hidden md:block h-[100vh] w-[300px]">
-            <Siderbar />
-          </div>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-[#020818]`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={true}
+          storageKey="dashboard-theme">
+          {children}
 
-          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
-        </div>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
